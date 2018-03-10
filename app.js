8d9df6d9524a1a0be7d2d40414cf1236447b8101
manoj kumar <manoj@w3dev.in>
2018-03-10 19:23:31 +0530

    More working

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const SendOtp = require('sendotp');
 const sendOtp = new SendOtp('201900AZxKhMho5aa3405d');
 
 
-sendOtp.send("+917011267648", "MANOJKS", SendOtp.generateOtp(), function(error, data, response) {
+sendOtp.send("+919718493150", "MANOJKS", SendOtp.generateOtp(), function(error, data, response) {
     console.log(data);
 });
