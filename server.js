fb92abe82e0daf047e051da969f80543fc61063e
manoj2017 <me.mk.1024@gmail.com>
2018-06-03 04:46:48 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,10 +5,19 @@ var express = require("express");
 var app = express();
 
 
+/* requiring config file starts*/
 require('./middleware/config.js')(app);
+/* requiring config file ends*/
 
+
+/* requiring config db.js file starts*/
 require("./middleware/db.js");
+/* requiring config db.js file ends*/
 
+
+/* 
+requiring config db.js file starts. This files handles the all the Routes for this application.
+*/
 require('./middleware/routes.js')(app);
 
 
